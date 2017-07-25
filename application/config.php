62eef0803b65f8a45b2f6b6798baa79c8d2ae247
lizongdi <240990281@qq.com>
2017-07-25 18:39:52 +0800

    登录与退出

--- application/config.php
+++ application/config.php
@@ -17,7 +17,7 @@ return [
     // 应用命名空间
     'app_namespace'          => 'app',
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true,
     // 应用Trace
     'app_trace'              => false,
     // 应用模式状态
@@ -125,7 +125,7 @@ return [
         // 模板引擎类型 支持 php think 支持扩展
         'type'         => 'Think',
         // 模板路径
-        'view_path'    => '',
+        'view_path'    => './static/',
         // 模板后缀
         'view_suffix'  => 'html',
         // 模板文件名分隔符
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,58 @@
 <?php
 namespace app\index\controller;
+use think\Request;
+use think\Db;
+use think\Controller;
 
-class Index
+class Index extends Controller
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        if (Request::instance()->isAjax())
+        {           
+            $username = input('post.username');
+            $password = input('post.password');
+            $where = array('username'=>$username,'password'=>md5($password),'status'=>1);
+            $user = Db::table('admin')->field("id,username,nick_name,mobile,login_num,group_id")->where($where)->find();
+            if (empty($user)||!$user)
+            {
+                return  json(['status'=>100,'message'=>'用户名或者密码错误']);
+            }else
+            {
+                $update['login_ip'] = Request::instance()->ip();
+                $update['login_time'] = time();
+                $update['login_num'] = $user['login_num']+1;
+                Db::table('admin')->where("id = {$user['id']}")->update($update);
+                session('admin_user', $user);     
+               return json(['status'=>0,'message'=>'登录成功']);
+            }
+        }else
+        {
+            return view('login');
+        }      
+    }
+    /**
+     * 後台首頁
+     * ***/
+    public function home()
+    {
+        if (session('?admin_user'))
+        {
+            $user = session('admin_user');
+            $username = empty($user['nick_name']) ? $user['username']:$user['nick_name'];
+            $data['username'] = $username;
+            return view('index',$data);
+        }else
+        {
+            return view('login');
+        }
+    }
+    public function loginOut()
+    {
+        if (session('?admin_user'))
+        {
+            session(null);
+        }
+        $this->success('退出成功', 'index/index');
     }
 }
