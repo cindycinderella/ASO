6584ef04004fc33ba8e2cb0ba5b1844da35663c6
lizongdi <240990281@qq.com>
2017-08-24 12:52:50 +0800

    随机语句修改

--- application/index/controller/Access.php
+++ application/index/controller/Access.php
@@ -322,7 +322,7 @@ class Access extends Controller {
                 $sql = "SELECT	* FROM	material WHERE 	id >= (
                 (	SELECT MAX(id)	FROM	material	WHERE	`status` = 1	AND type = '{$type['id']}') -
                 (SELECT	MIN(id)	FROM	material	WHERE	`status` = 1	AND type = '{$type['id']}'	)
-                ) * RAND()*100 + (SELECT	MIN(id)	FROM		material	WHERE	`status` = 1	AND type = '{$type['id']}') AND `status` = 1
+                ) * RAND()  + (SELECT	MIN(id)	FROM		material	WHERE	`status` = 1	AND type = '{$type['id']}') AND `status` = 1
                 AND type = '{$type['id']}' LIMIT 1";
                 $ontent = Db::query($sql);
                 $titleList[$kk]['content'] = $ontent[0]['content'];
